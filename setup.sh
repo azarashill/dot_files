1d8c10717d0d94e3772d8580ac8c571c5b34f9a3
azarashill <itahashitakushi@itahashitakushinoMacBook-Pro.local>
2019-02-20 20:10:04 +0900

    gitconfig

--- setup.sh
+++ setup.sh
@@ -28,37 +28,38 @@ git clone https://github.com/Shougo/dein.vim.git \
     ~/.vim/dein/repos/github.com/Shougo/dein.vim
 
 
-# インストール
-cd fonts
-./install.sh
-
-# 必要なければcloneファイルを削除
-cd ..
-rm -rf fonts
 
 #zsh-completions
 git clone git://github.com/zsh-users/zsh-completions.git
 
 cd $(dirname $0)
-echo "$PWD/zsh-completions" >> ./.zshrc
+echo "fpath=($PWD/zsh-completions $fpath)" >> ./.zshrc
 echo "autoload -U compinit" >> ./.zshrc
 echo "compinit -u" >> ./.zshrc
 
 
 #zsh
 #なんかエラー出るから最後(brewに関してはアレ)
-brew install zsh
-chsh -s /usr/local/bin/zsh
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+#brew install zsh
+#chsh -s /usr/local/bin/zsh
+#git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+#setopt EXTENDED_GLOB
+#for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+#  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+#done
 
-echo "zstyle ':prezto:module:prompt' theme 'agnoster'" >> ~/.zpreztorc
+#echo "zstyle ':prezto:module:prompt' theme 'agnoster'" >> ~/.zpreztorc
 
-source ~/.zpreztorc
+#source ~/.zpreztorc
 
 # cloneする
-git clone https://github.com/powerline/fonts.git --depth=1
+#git clone https://github.com/powerline/fonts.git --depth=1
+
+# インストール
+#cd fonts
+#./install.sh
+
+# 必要なければcloneファイルを削除
+#cd ..
+#rm -rf fonts
